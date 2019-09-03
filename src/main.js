d8e08729f38fe6db1d4ad7d9b024fd71ad5459dc
WeiZaiHenMang <774717622@qq.com>
2019-09-03 20:54:57 +0800

    '登陆页面布置完成'

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import '../src/style/index.less'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  Vant,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <van-nav-bar title="登陆" />
+    <van-cell-group>
+      <van-field v-model="value" left-icon="manager" placeholder="请输入手机号" />
+      <van-field v-model="value" left-icon="lock" placeholder="请输入验证码">
+        <van-button slot="button" round type="danger" color="#ccc" size="small">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="box_but">
+      <van-button type="primary" color="#3e9df8" size="large">登陆</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
+
+<style>
+.van-cell-group {
+  margin-bottom: 30px;
+}
+.box_but {
+  margin: 0px 20px;
+}
+</style>
