534d6e22af0d95e431dac16be8621b56ba458723
WeiZaiHenMang <774717622@qq.com>
2019-09-04 17:15:07 +0800

    '首页路由完成'

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import table from '../views/tabel.vue'
+import home from '../views/home/home.vue'
 
 Vue.use(Router)
 
@@ -7,10 +9,15 @@ export default new Router({
   routes: [{
     path: '/login',
     name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
             import(/* webpackChunkName: "about" */ '../views/login.vue')
+  }, {
+    path: '/',
+    name: 'table',
+    component: table,
+    children: [{
+      path: '',
+      component: home
+    }]
   }]
 })
---
+++ src/views/home/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>首页内容区</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
