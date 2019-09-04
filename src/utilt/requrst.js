314f4e149ea9af3cd6e3be91c762150d9429cc6d
WeiZaiHenMang <774717622@qq.com>
2019-09-04 09:24:04 +0800

    '建立axios实例及大数据解决方案'

---
+++ src/utilt/requrst.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import jsonbig from 'json-bigint'
+
+// 设置axios实例
+const instance = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn',
+  timeout: 1000
+})
+
+// 大数据转换
+instance.defaults.transformResponse = [function (data) {
+  try {
+    // data 数据可能不是标准的 JSON 格式字符串，否则会导致 JSONbig.parse(data) 转换失败报错
+    return jsonbig.parse(data)
+  } catch (err) {
+    // 无法转换的数据直接原样返回
+    return data
+  }
+}]
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  console.log(response)
+  return response
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error)
+})
+
+export default instance
--- src/views/login.vue
+++ src/views/login.vue
@@ -2,23 +2,37 @@
   <div>
     <van-nav-bar title="登陆" />
     <van-cell-group>
-      <van-field v-model="value" left-icon="manager" placeholder="请输入手机号" />
-      <van-field v-model="value" left-icon="lock" placeholder="请输入验证码">
+      <van-field v-model="user.mobile" left-icon="manager" placeholder="请输入手机号" />
+      <van-field v-model="user.code" left-icon="lock" placeholder="请输入验证码">
         <van-button slot="button" round type="danger" color="#ccc" size="small">发送验证码</van-button>
       </van-field>
     </van-cell-group>
     <div class="box_but">
-      <van-button type="primary" color="#3e9df8" size="large">登陆</van-button>
+      <van-button type="primary" color="#3e9df8" size="large" @click="landing">登陆</van-button>
     </div>
   </div>
 </template>
 
 <script>
+import { login } from '../api/user'
 export default {
   name: 'login',
   data () {
     return {
-      value: ''
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      }
+    }
+  },
+  methods: {
+    async landing () {
+      try {
+        const result = await login(this.user)
+        console.log(result)
+      } catch (error) {
+        console.log('错误')
+      }
     }
   }
 }
